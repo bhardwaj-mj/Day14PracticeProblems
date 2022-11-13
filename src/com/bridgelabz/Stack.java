7e68d62ae8a9420273db4291c7709d3e0bbbff3e
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-11-13 15:58:39 +0530

    Added: Peek And Pop method in stack class

--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -15,11 +15,22 @@ public class Stack<E> {
     public void print() {
         linkedList.show();
     }
+    public void peek(){
+        System.out.println(linkedList.head.getKey());
+    }
+
+    public void popStack(){
+        linkedList.pop();
+    }
     public static void main(String[] args) {
         Stack<Integer> stack1 = new Stack<>();
         stack1.push(70);
         stack1.push(30);
         stack1.push(56);
+        stack1.print();
+
+        stack1.peek();
+        stack1.popStack();
         stack1.print();
 
     }
